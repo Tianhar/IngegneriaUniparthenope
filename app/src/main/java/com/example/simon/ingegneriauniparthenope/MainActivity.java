2519bcb1054be00ff3f650df6cc83519ebb85a1b
colviem <colviem@gmail.com>
2018-05-03 13:49:33 +0200

    Aggiunto menù

--- app/src/main/java/com/example/simon/ingegneriauniparthenope/MainActivity.java
+++ app/src/main/java/com/example/simon/ingegneriauniparthenope/MainActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -55,5 +58,34 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.ITEM_1:
+
+                startActivity(new Intent(MainActivity.this, MapsActivity.class));
+
+                break;
+            case R.id.ITEM_2:
+            /*
+                Codice di gestione della voce ITEM_2 ALERT CON INFO REALIZZAZIONE APP AUTORI ECC
+             */
+                break;
+
+            case R.id.ITEM_3:
+
+                MainActivity.this.finish();
+
+        }
+        return false;
     }
 }
