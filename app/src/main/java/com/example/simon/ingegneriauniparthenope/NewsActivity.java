4c38bfc023b564c4775053bde5a24540bd623b6c
Tianhar <simoneardiero@hotmail.it>
2018-05-06 13:48:35 +0200

    Modifiche del 06/05/2018 parte 2
    -Piccolo fix a NewsDownloader

--- app/src/main/java/com/example/simon/ingegneriauniparthenope/NewsActivity.java
+++ app/src/main/java/com/example/simon/ingegneriauniparthenope/NewsActivity.java
@@ -2,13 +2,13 @@ package com.example.simon.ingegneriauniparthenope;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import org.jsoup.Jsoup;
+import android.widget.TextView;
+
+import static com.example.simon.ingegneriauniparthenope.MainActivity.newsd;
+
 
 public class NewsActivity extends AppCompatActivity {
+
 
     protected void onCreate(Bundle SavedInstanceState) {
         super.onCreate(SavedInstanceState);
@@ -16,36 +16,21 @@ public class NewsActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        //TODO - DA MIGLIORARE
+        TextView result = (TextView) findViewById(R.id.result);
 
+        result.setText(newsd.builder.toString());
+
+        //TODO - DA MIGLIORARE
+/*
         final WebView webview = findViewById(R.id.newsView);
         webview.getSettings().setJavaScriptEnabled(true);
         webview.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
         webview.getSettings().setSupportZoom(true);
         webview.getSettings().setTextZoom(120);
         webview.setTextAlignment(View.TEXT_ALIGNMENT_VIEW_START);
-        webview.setWebViewClient(new WebViewClient() {
-
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                webview.loadUrl("javascript:(function() { " +
-                        "document.getElementById('header').style.display='none'; " +
-                        "document.getElementById('sidebar').style.display='none'; " +
-                        "document.getElementById('content').style.display='block'; " +
-                        "})()");
-
-            }
-        });
-        webview.loadUrl("http://www.ingegneria.uniparthenope.it/avvisi.php");
-
-        webview.setTextAlignment(View.TEXT_ALIGNMENT_VIEW_START);
-
-
-
-
-
+        webview.loadData(newsd.builder.toString(), "text/html; charset=utf-8", "UTF-8");
+*/
     }
-
 
 }
 
---
+++ app/src/main/java/com/example/simon/ingegneriauniparthenope/NewsDownloader.java
@@ -0,0 +1,51 @@
+package com.example.simon.ingegneriauniparthenope;
+
+import android.os.AsyncTask;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+
+public class NewsDownloader extends AsyncTask<Void, Void, Void> {
+    public static StringBuilder builder = new StringBuilder();
+
+    @Override
+    protected Void doInBackground(Void... voids) {
+        try {
+            Document doc = Jsoup.connect("http://www.ingegneria.uniparthenope.it/index.php?page=news").get();
+            String title = doc.title();
+            Element content = doc.getElementById("content");
+            Elements newsc = content.getElementsByClass("news");
+
+            builder.append("News " + title).append("\n\n");
+
+            for (Element link : newsc) {
+                Elements dates = link.getElementsByClass("data");
+                int darimuovere = dates.text().length();
+                Elements titles = link.getElementsByTag("h4");
+                Elements contenuto = link.getElementsByTag("p");
+                String cont = contenuto.text().replaceAll("’", "'");
+                Elements list = link.getElementsByTag("li");
+
+                builder.append("\n").append("-News del ").append(dates.text()).append("\n")
+                        .append("\n").append(titles.text().substring(darimuovere + 1)).append("\n")
+                        .append("\n").append(cont).append("\n");
+                int ii = 1;
+                for (Element liste : list) {
+                    builder.append("\n").append("" + ii + "- " + liste.text()).append("\n");
+                    ii++;
+                }
+                builder.append("\n\n");
+
+            }
+        } catch (IOException e) {
+            builder.append("Error : ").append(e.getMessage()).append("\n");
+        }
+        return null;
+    }
+
+}
