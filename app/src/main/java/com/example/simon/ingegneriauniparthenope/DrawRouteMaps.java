c69a9f4f9bd49d9382e41cf6e9ca01f5a67faf11
colviem <colviem@gmail.com>
2018-05-25 13:38:36 +0200

    Mappa: Git di classi per il calcolo percorso

---
+++ app/src/main/java/com/example/simon/ingegneriauniparthenope/DrawRouteMaps.java
@@ -0,0 +1,30 @@
+package com.example.simon.ingegneriauniparthenope;
+
+import android.content.Context;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+
+
+public class DrawRouteMaps {
+
+    private static DrawRouteMaps instance;
+    private Context context;
+
+    public static DrawRouteMaps getInstance(Context context) {
+        instance = new DrawRouteMaps();
+        instance.context = context;
+        return instance;
+    }
+
+    public static Context getContext() {
+        return instance.context;
+    }
+
+    public DrawRouteMaps draw(LatLng origin, LatLng destination, GoogleMap googleMap) {
+        String url_route = FetchUrl.getUrl(origin, destination);
+        DrawRoute drawRoute = new DrawRoute(googleMap);
+        drawRoute.execute(url_route);
+        return instance;
+    }
+}
